313453666baf968024aa0801ced7a253f2654ca9
Sean Vela <sean_vela@yahoo.com>
2024-07-25 09:33:15 -0500

    updated home page

--- Home.py
+++ Home.py
@@ -66,14 +66,14 @@ col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
 
 df = pandas.read_csv('data.csv', sep=';')
 with col3:
-    for index, row in df[:7].iterrows():
+    for index, row in df[:6].iterrows():
         st.header(row["title"])
         st.write(row["description"])
         st.image("images/" + row["image"])
         st.write(f"[Source code]({row['url']})")
 
 with col4:
-    for index, row in df[7:].iterrows():
+    for index, row in df[6:].iterrows():
         st.header(row["title"])
         st.write(row["description"])
         st.image("images/" + row["image"])
